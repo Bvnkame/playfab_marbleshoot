30685aec32a45a7dee03515405bf9b8e7dbcebcf
Bùi Văn Nam <bvnbase@gmail.com>
2016-12-18 00:24:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 // parameter of the ExecuteCloudScript API.
 // (https://api.playfab.com/Documentation/Client/method/ExecuteCloudScript)
 // "context" contains additional information when the Cloud Script function is called from a PlayStream action.
-handlers.helloWorld = function (args, context) {
+handlers.getFriendsList = function (args, context) {
     
     var playerFriends = server.GetFriendsList (
         {
             IncludeFacebookFriends : true
         }
     );
-    return { messageValue: message };
+    
+    log.debug(playerFriends);
+    //return { messageValue: message };
 }
